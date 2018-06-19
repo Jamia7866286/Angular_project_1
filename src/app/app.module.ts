e874ef4996ed83114c7b9e891a4caef0c57fb6e5
Naseem Ahmad <naseemahmadjmi96>
2018-06-19 12:38:06 +0530

    changes by Naseem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
-
 import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
+import { AppRoutingModule } from './app.routing.module';
 
 
 @NgModule({
@@ -14,7 +13,8 @@ import { HerosComponent } from './heros/heros.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { routes,RouterModule, RoutesRecognized } from '@angular/router';
+import { AppComponent } from './app.component';
+
+
+const routes1: routes= [
+    {
+        path: 'test', component: AppComponent
+    },
+]
+@NgModule({
+    exports: [
+        RouterModule
+    ],
+    imports: [
+        RouterModule.forRoot(routes1);
+    ],
+})
+export class AppRoutingModule{
+
+}
